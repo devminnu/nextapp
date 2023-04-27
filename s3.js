d2e614e9b52f1ac32b28db28779bcccf54ce1c1b
Minhajuddin Quazi <minhajqazi@live.com>
2023-04-27 12:18:56 +0530

    Update s3.js

--- s3.js
+++ s3.js
@@ -24,3 +24,46 @@ const uploadResult = await s3.upload({
 }).promise();
 
 console.log('File uploaded:', uploadResult.Location);
+
+
+
+
+
+const { STSClient, AssumeRoleCommand } = require("@aws-sdk/client-sts");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { Readable } = require("stream");
+
+// Instantiate an STS client using the default AWS configuration
+const stsClient = new STSClient();
+
+// Assume the IAM role specified by the ARN
+const assumeRoleResult = await stsClient.send(new AssumeRoleCommand({
+  RoleArn: 'arn:aws:iam::123456789012:role/MyECSRole',
+  RoleSessionName: 'MySessionName'
+}));
+
+// Create an S3 client using the temporary credentials
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: assumeRoleResult.Credentials.AccessKeyId,
+    secretAccessKey: assumeRoleResult.Credentials.SecretAccessKey,
+    sessionToken: assumeRoleResult.Credentials.SessionToken
+  }
+});
+
+// Read the contents of the file into a Readable stream
+const fileData = new Readable({
+  read() {
+    this.push("Hello, World!");
+    this.push(null);
+  }
+});
+
+// Upload a file to S3 using the temporary credentials
+const uploadResult = await s3Client.send(new PutObjectCommand({
+  Bucket: 'my-bucket',
+  Key: 'path/to/file',
+  Body: fileData
+}));
+
+console.log('File uploaded:', uploadResult.Location);
