92cd7169d3b7329c0e54b4479ba4f6d28bb19500
Minhajuddin Quazi <minhajqazi@live.com>
2023-07-23 23:18:32 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,39 @@
-transform: function(content) {
-          const stringToMatch = '{{stringToMatch}}';
-          const contentToAdd = '{{contentToAdd}}';
-          const index = content.indexOf(stringToMatch);
-          if (index === -1) {
-            throw new Error(`String "${stringToMatch}" not found in file`);
-          }
-          return content.slice(0, index + stringToMatch.length) +
-            '\n' + contentToAdd +
-            '\n' + content.slice(index + stringToMatch.length);
-        }
+// client.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "server-ip:8080")
+	if err != nil {
+		fmt.Println("Error connecting to the server:", err.Error())
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to the server.")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = strings.TrimSpace(input)
+
+		if input == "exit" {
+			break
+		}
+
+		_, err := conn.Write([]byte(input))
+		if err != nil {
+			fmt.Println("Error sending data:", err.Error())
+			break
+		}
+	}
+}
