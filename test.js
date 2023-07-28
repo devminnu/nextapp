92c1b086f7c4d906620b7531c38cabcc248740a9
Minhajuddin Quazi <minhajqazi@live.com>
2023-07-28 11:19:01 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-	// Use reflection to loop over the struct fields and update if value is "Hello"
-	reflectValue := reflect.ValueOf(&myStruct).Elem()
-	reflectType := reflectValue.Type()
+// Convert IPv4 to IPv6 (IPv4-mapped IPv6 address)
+func ipv4ToIPv6(ipv4 net.IP) net.IP {
+	ipv6 := make(net.IP, net.IPv6len)
+	// Set the first 10 bytes to 0x00
+	copy(ipv6, net.IPv6zero[:10])
+	// Set bytes 11 and 12 to 0xff
+	copy(ipv6[10:], net.IPv6unspecified[:2])
+	// Copy the last 4 bytes from the IPv4 address into the last 4 bytes of the IPv6 representation
+	copy(ipv6[12:], ipv4.To4())
 
-	for i := 0; i < reflectValue.NumField(); i++ {
-		fieldValue := reflectValue.Field(i)
-		if fieldValue.Kind() == reflect.String && fieldValue.String() == "Hello" {
-			newValue := "Updated"
-			fieldValue.SetString(newValue)
-		}
-	}
+	return ipv6
+}
