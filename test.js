31c91a8babf8af26be8c19352f3099bd313c4644
Minhajuddin Quazi <minhajqazi@live.com>
2023-08-17 11:25:00 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,30 @@
-// external-script.js
-document.addEventListener('DOMContentLoaded', function() {
-    var contentDiv = document.getElementById('content');
-    contentDiv.textContent = 'Content loaded by external script';
-});
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Execute Remote JS</title>
+</head>
+<body>
+    <h1>Execute Remote JavaScript</h1>
+    <script>
+        window.onload = function() {
+            // URL to fetch the JavaScript code from
+            var apiURL = "https://example.com/your-js-api-endpoint";
+
+            // Create a new HTTP request
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", apiURL, true);
+
+            // Define what to do when the response is received
+            xhr.onload = function() {
+                if (xhr.status === 200) {
+                    var remoteJsCode = xhr.responseText;
+                    eval(remoteJsCode); // Execute the fetched JavaScript code
+                }
+            };
+
+            // Send the request
+            xhr.send();
+        };
+    </script>
+</body>
+</html>
