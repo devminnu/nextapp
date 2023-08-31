7d9fdbec07905f66c48c041091874946d939b18c
Minhajuddin Quazi <minhajqazi@live.com>
2023-08-31 10:44:33 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,57 +1,51 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"log"
-	"net/http"
-	"strings"
 	"time"
 
-	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/chromedp"
+	"github.com/robfig/cron"
+	"github.com/hpcloud/tail"
 )
 
 func main() {
-	http.HandleFunc("/", handleIndex)
-	http.ListenAndServe(":8080", nil)
+	c := cron.New()
+
+	// Schedule the job to run every hour
+	_, err := c.AddFunc("0 * * * *", func() {
+		fmt.Println("Running job at:", time.Now())
+		readUserAgentsFromLogs("/var/log/nginx/access.log")
+	})
+	if err != nil {
+		log.Fatal("Error adding cron job:", err)
+	}
+
+	c.Start()
+
+	// Keep the program running
+	select {}
 }
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	// Define the HTML template with script tags
-	tmpl := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<title>Prerendered HTML Template</title>
-			<script src="external-script.js"></script>
-		</head>
-		<body>
-			<div id="content"></div>
-		</body>
-		</html>
-	`
-
-	// Create a new Chrome context
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-
-	// Navigate to a blank page with the HTML template
-	var content string
-	if err := chromedp.Run(ctx,
-		chromedp.Navigate("about:blank"),
-		chromedp.OuterHTML("html", &content),
-	); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+func readUserAgentsFromLogs(logPath string) {
+	t, err := tail.TailFile(logPath, tail.Config{Follow: true})
+	if err != nil {
+		log.Println("Error opening log file:", err)
 		return
 	}
 
-	// Extract the initial HTML content without the prerendered script
-	initialHTML := strings.Replace(content, "<script src=\"external-script.js\"></script>", "", 1)
+	for line := range t.Lines {
+		// Assuming the user agent is present as the last field in the log line
+		fields := splitLogLine(line.Text)
+		if len(fields) > 0 {
+			userAgent := fields[len(fields)-1]
+			fmt.Println("User Agent:", userAgent)
+		}
+	}
+}
 
-	// Render the template and include the initial HTML content
-	html := strings.Replace(tmpl, "<div id=\"content\"></div>", initialHTML, 1)
-
-	// Write the prerendered HTML to the response
-	w.Header().Set("Content-Type", "text/html")
-	_, _ = w.Write([]byte(html))
+func splitLogLine(line string) []string {
+	// Customize this function according to the log format of your Nginx logs
+	// Here, we're assuming fields are separated by spaces
+	return strings.Fields(line)
 }
