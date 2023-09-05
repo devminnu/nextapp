d46df0915022c7ef324d51bcf52dfd93c104ab57
Minhajuddin Quazi <minhajqazi@live.com>
2023-09-06 00:22:35 +0530

    Update test.js

--- test.js
+++ test.js
@@ -6,10 +6,20 @@ app.use(express.json());
 
 app.post('/render', (req, res) => {
     const html = req.body.html; // HTML content with nested script tags
-    const dom = new JSDOM(html);
+    const dom = new JSDOM(html, { runScripts: 'dangerously' });
 
-    // Execute JavaScript within the DOM
-    dom.window.evalScripts();
+    // Extract and evaluate script tags
+    const scriptElements = dom.window.document.querySelectorAll('script');
+    scriptElements.forEach((scriptElement) => {
+        if (scriptElement.textContent) {
+            // Evaluate the script content
+            try {
+                dom.window.eval(scriptElement.textContent);
+            } catch (error) {
+                console.error('Error evaluating script:', error);
+            }
+        }
+    });
 
     res.send(dom.serialize());
 });
