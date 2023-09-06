252ff06bf45d8df021deedf44abc7b4cd714a1e7
Minhajuddin Quazi <minhajqazi@live.com>
2023-09-06 20:06:17 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,44 @@
-const express = require('express');
-const { JSDOM } = require('jsdom');
+// Define the API URL that returns the ad HTML content
+const apiUrl = 'https://example.com/api/get-ad';
 
-const app = express();
-app.use(express.json());
+// Function to fetch and insert the ad content
+async function loadAd() {
+    try {
+        // Make an API request to get the ad HTML content
+        const response = await fetch(apiUrl);
 
-app.post('/render', (req, res) => {
-    const html = req.body.html; // HTML content with nested script tags
-    const dom = new JSDOM(html, { runScripts: 'dangerously' });
-
-    // Extract and evaluate script tags
-    const scriptElements = dom.window.document.querySelectorAll('script');
-    scriptElements.forEach((scriptElement) => {
-        if (scriptElement.textContent) {
-            // Evaluate the script content
-            try {
-                dom.window.eval(scriptElement.textContent);
-            } catch (error) {
-                console.error('Error evaluating script:', error);
-            }
+        if (!response.ok) {
+            throw new Error(`API request failed with status ${response.status}`);
         }
-    });
 
-    res.send(dom.serialize());
-});
+        // Parse the response as text
+        const adHtml = await response.text();
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Node.js service is listening on port ${PORT}`);
-});
+        // Create a div element to hold the ad content
+        const adContainer = document.getElementById('ad-container');
+        const adWrapper = document.createElement('div');
+
+        // Set the HTML content of the wrapper div with the ad HTML
+        adWrapper.innerHTML = adHtml;
+
+        // Append the wrapper div to the ad container
+        adContainer.appendChild(adWrapper);
+
+        // After inserting the HTML, you might want to execute any JavaScript in the ad content
+        // For example, you can execute scripts within the inserted content using a function like this:
+        function executeScripts() {
+            const scripts = adWrapper.querySelectorAll('script');
+            scripts.forEach((script) => {
+                const newScript = document.createElement('script');
+                newScript.text = script.textContent;
+                script.parentNode.replaceChild(newScript, script);
+            });
+        }
+        executeScripts();
+    } catch (error) {
+        console.error('Error loading ad:', error);
+    }
+}
+
+// Call the loadAd function to fetch and insert the ad content
+loadAd();
