d99db4b3b3d4aaa23fdf1eff7a0e47658c2ccb30
Minhajuddin Quazi <minhajqazi@live.com>
2023-09-14 13:03:19 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,47 +1,36 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"regexp"
-	"strings"
+    "fmt"
+    "io/ioutil"
+    "math/rand"
+    "net/http"
+    "os"
+    "time"
 )
 
 func main() {
-	logFilePath := "/var/log/nginx/access.log"
-	err := readUserAgentsFromLogs(logFilePath)
-	if err != nil {
-		log.Fatal("Error:", err)
-	}
+    http.HandleFunc("/js", generateJS)
+    http.ListenAndServe(":8080", nil)
 }
 
-func readUserAgentsFromLogs(logPath string) error {
-	file, err := os.Open(logPath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+func generateJS(w http.ResponseWriter, r *http.Request) {
+    // Generate a random file name
+    rand.Seed(time.Now().UnixNano())
+    randStr := fmt.Sprintf("%d.js", rand.Intn(10000))
 
-	scanner := bufio.NewScanner(file)
-	userAgentRegex := regexp.MustCompile(`"([^"]*)"`)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fields := strings.Fields(line)
-		if len(fields) > 11 {
-			userAgentField := fields[11]
-			userAgentMatches := userAgentRegex.FindStringSubmatch(userAgentField)
-			if len(userAgentMatches) > 1 {
-				userAgent := userAgentMatches[1]
-				fmt.Println("User Agent:", userAgent)
-			}
-		}
-	}
+    // Create a new JavaScript file
+    jsContent := []byte(`console.log("This is a dynamically generated JavaScript file.");`)
+    err := ioutil.WriteFile(randStr, jsContent, 0644)
+    if err != nil {
+        http.Error(w, "Failed to create JavaScript file", http.StatusInternalServerError)
+        return
+    }
+    defer os.Remove(randStr) // Clean up the file when done
 
-	if err := scanner.Err(); err != nil {
-		return err
-	}
+    // Serve the generated JavaScript file
+    http.ServeFile(w, r, randStr)
 
-	return nil
+    // Optionally, set response headers to indicate that it's a JavaScript file
+    w.Header().Set("Content-Type", "application/javascript")
 }
